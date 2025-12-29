4724ffebee3db6331eec9115419366a6ccd5239c
hammad373 <sadafkhn861@gmail.com>
2025-12-29 15:36:07 +0530

    hi

---
+++ a.py
@@ -0,0 +1,24 @@
+d1=18
+m1=11
+y1=2018
+print("I was born in...", d1, m1, y1)
+
+d1=9
+m1=12
+y1=1996
+print("Lucas was born in...", d1, m1, y1)
+
+d1=2
+m1=4
+y1=2017
+print("Ameera was born in...", d1, m1, y1)
+
+d1=7
+m1=9
+y1=2007
+print("Mike was born in...", d1, m1, y1)
+
+d1=17
+m1=10
+y1=1999
+print("Hamdan was born in...", d1, m1, y1)
